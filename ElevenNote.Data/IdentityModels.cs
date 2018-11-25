1a4fb45949b9e5264bf889894a5aa69b14302893
Stephanie Mizelle <samizelle@gmail.com>
2018-11-24 20:08:37 -0500

    commit Note.cs build

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -54,7 +54,7 @@ namespace ElevenNote.Data
         }
     }
 
-    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
     {
         public IdentityUserRoleConfiguration()
         {

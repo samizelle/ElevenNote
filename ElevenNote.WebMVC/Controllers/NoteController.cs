f14d89deeb71b6b446a3169d1938e343cc041b54
Stephanie Mizelle <samizelle@gmail.com>
2018-11-24 21:26:59 -0500

    commit Index.cshtml

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ElevenNote.Models;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}

17aa2f7594d6eeccb4edb575f648b5b76688e5c4
Stephanie Mizelle <samizelle@gmail.com>
2018-11-26 22:13:05 -0500

    commit API Controller build

---
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+
+namespace ElevenNote.WebApi.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = NoteService.GetNotes();
+            return Ok(notes);
+        }
+
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        }
+    }
+}
